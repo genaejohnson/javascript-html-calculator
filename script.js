74fef7a63dfd509e762a6aee541d7f386bb64ecd
Genae Johnson <gsjohnson@wlapcs.org>
2017-02-15 21:39:10 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function cleardisplay(){
+    document.getElementById("display-bar").innerHTML = "";
+}
+
+function display(userInput){
+    document.getElementById("display-bar").innerHTML += userInput;
+}
+
+function calculate(){
+var answer = eval( document.getElementById("display-bar").innerHTML);
+   document.getElementById("display-bar").innerHTML = answer;   
+}
+
