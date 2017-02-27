4b9d6801c48ac678cc1ebd8c01ccb866691acb17
Genae Johnson <gsjohnson@wlapcs.org>
2017-02-27 18:07:41 +0000

    added styling

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ function display(userInput){
 
 function calculate(){
 var answer = eval( document.getElementById("display-bar").innerHTML);
-   document.getElementById("display-bar").innerHTML = answer;   
+   document.getElementById("display-bar").innerHTML  = answer;   
 }
 
